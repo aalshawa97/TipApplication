51b93929b533c190d663f25ae7725a6075e6e9d6
aalshawa97 <abdullahalshawa@gmail.com>
2021-11-08 09:04:44 -0800

    Functional

--- app/src/main/java/com/example/tippy/MainActivity.kt
+++ app/src/main/java/com/example/tippy/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.tippy
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import org.w3c.dom.Text
 
 private const val TAG = "MainActivity"
 private const val INITIAL_TIP_PERCENT = 15
+private lateinit var etBase : EditText
+private lateinit var seekBar : SeekBar
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        etBase = findViewById(R.id.etBase)
+        seekBar = findViewById(R.id.seekBar)
         seekBar.progress = INITIAL_TIP_PERCENT
         val tipPercent = findViewById<TextView>(R.id.tvTipPercent)
         tipPercent.text = "$INITIAL_TIP_PERCENT"
@@ -31,7 +37,34 @@ class MainActivity : AppCompatActivity() {
                 // TODO Auto-generated method stub
                 Log.i(TAG, "onProgressChanged $progress")
                 tipPercent.text = "$progress%"
+                computeTipAndTotal()
             }
         })
+
+        etBase.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "After text changed $p0")
+                computeTipAndTotal()
+            }
+
+        })
+    }
+
+    private fun computeTipAndTotal(){
+        //Get the value of the base and tip percent
+        val baseAmount = etBase.text.toString().toDouble()
+        val tipPercent = seekBar.progress
+        val tipAmount = baseAmount * tipPercent / 100
+        val totalAmount = baseAmount + tipAmount
+        val tvTipAmount : TextView = findViewById(R.id.tvTipTotal)
+        tvTipAmount.text = tipAmount.toString()
+        val tvBaseTotal = findViewById<TextView>(R.id.tvBaseTotal)
+        tvBaseTotal.text = totalAmount.toString()
     }
 }
